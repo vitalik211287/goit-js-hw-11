2b7448d2bdebb8ff564a8f618f772afe335f5620
Vitalik Poliakov <vitalik211287@gmail.com>
2022-09-26 22:43:53 +0300

    refactoring: async await

--- src/index.js
+++ src/index.js
@@ -7,56 +7,55 @@ const onLoadMore = document.querySelector('.load-more');
 const newApiServece = new NewApiServece();
 
 onSearch.addEventListener('submit', submitHandler);
+onLoadMore.style.visibility = 'hidden';
 
-function submitHandler(event) {
+async function submitHandler(event) {
   event.preventDefault();
-  newApiServece.query = event.currentTarget.elements.searchQuery.value;
+  const inputValue = event.currentTarget.elements.searchQuery.value
+  if(!inputValue){
+    return
+  }
+  newApiServece.query = inputValue;
   newApiServece.resetPage();
   cleanerEl();
-  newApiServece
-    .fetchGallery()
-    .then(({ data }) => {
-      if (data.hits.length === 0) {
-        cleanerEl();
-        Notify.info(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-        return;
-      }
-      Notify.info(`Hooray! We found ${data.total} images.`);
+  try {
+    const { data } = await newApiServece.fetchGallery();
 
-      renderGalleryList(data.hits);
-    })
-    .catch(error => {
-      Notify.failure(`Oops! Something went wrong...`);
-      console.error(error);
-    });
-  // divOnLoadMore.append('onLoadMore');
-  onLoadMore.style.visibility = 'visible';
+    if (data.hits.length === 0) {
+      cleanerEl();
+      Notify.info(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+    Notify.info(`Hooray! We found ${data.total} images.`);
+    onLoadMore.style.visibility = 'visible';
+    renderGalleryList(data.hits);
+  } catch (error) {
+    Notify.failure(`Oops! Something went wrong...`);
+    console.error(error);
+  }
 }
 
-// onLoadMore.remove();
-onLoadMore.style.visibility = 'hidden';
-onLoadMore.addEventListener('click', event => {
+
+
+onLoadMore.addEventListener('click', async event => {
   event.preventDefault();
-  newApiServece
-    .fetchGallery()
-    .then(({ data }) => {
-      if (data.hits.length === 0) {
-        cleanerEl();
-        Notify.info(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      }
-      if (data.totalHits === newApiServece.page) {
-        Notify.info(
-          'We`re sorry, but you`ve reached the end of search results.'
-        );
-      }
-      console.log(newApiServece.query);
-      renderGalleryList(data.hits);
-    })
-    .catch(error => {
-      console.log(error);
-    });
+  try {
+    const { data } = await newApiServece.fetchGallery();
+    if (data.hits.length === 0) {
+      cleanerEl();
+      Notify.info(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+    if (data.totalHits === newApiServece.page) {
+      Notify.info('We`re sorry, but you`ve reached the end of search results.');
+    }
+   
+    renderGalleryList(data.hits);
+  } catch (error) {
+    Notify.failure(`Oops! Something went wrong...`);
+    console.error(error);
+  }
 });
